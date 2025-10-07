ebc9e6964e8bf9607c9f9e15129a9baf26c0e947
Bruno Pelatieri Goulart <brunopelatieri@gmail.com>
2025-10-07 00:13:12 -0300

    deploy page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
 
 export default function Home() {
   return (
-    <div>
-      Hello, project dapp!
-    </div>
+    <>
+      <div className="container px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-6">
+            <img
+              src="https://getbootstrap.com/docs/5.3/examples/heroes/bootstrap-docs.png"
+              className="d-block mx-lg-auto img-fluid"
+              alt="Bootstrap Themes"
+              width="700"
+              height="500"
+              loading="lazy"
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
